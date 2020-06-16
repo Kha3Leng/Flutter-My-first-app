8c33500b32090204533791547d21cc84b8458ae8
Easy Money <sai@Sai-MacBook-Pro.local>
2020-06-16 21:47:36 +0630

    Icon

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,12 @@ class Home extends StatelessWidget {
 //          image: AssetImage('asset/one.jpg'),
 //        ),
 //        child: Image.asset('asset/two.png'),
-          child: Image.network('https://images.unsplash.com/photo-1592035187437-47c0fe77a452?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1391&q=80'),
+//          child: Image.network('https://images.unsplash.com/photo-1592035187437-47c0fe77a452?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1391&q=80'),
+        child: Icon(
+          Icons.airport_shuttle,
+          color: Colors.lightBlue,
+          size: 50,
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
